93de7c5f4b7cfad3c7c08e8620e0440a02bdfd92
gewoehnlich <oomyse@gmail.com>
2025-01-06 02:25:20 +0500

    added something a little bit

--- handlers/files.sh
+++ handlers/files.sh
@@ -9,10 +9,18 @@ fi
 for filetype in $languages
 do
     file="$directory/$filename.$filetype"
+    echo $file
 
     if [[ ! -f "$file" ]]; then
         touch "$file"
     elif [[ ! -s $file ]]; then
-        
+        read -p "Seems like the $file file already exists!\nDo you want to rewrite the file with a clear template? (Y/n)" input
+
+        if [[ "$input" == "Y" ]]; then
+            echo "User entered Y."
+        else 
+            echo "User did not entered Y."
+        fi
     fi
 done
+
