e1148dc97dc35e505ab1c98a36c8ec610b66bb2a
gewoehnlich <oomyse@gmail.com>
2025-01-09 00:48:03 +0500

    rewrote everything i wrote in shell to python

--- env.py
+++ env.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import sys
 from handlers.init import setVariables
+from handlers.files import makeDirectoryAndFiles
+
 
 if __name__ == '__main__':
     problem_number, file_formats = setVariables(sys.argv)
     print(f"Problem number: {problem_number}")
     print(f"File formats: {file_formats}")
+    
+    makeDirectoryAndFiles(problem_number, file_formats)
 
---
+++ handlers/files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+from typing import List
+
+def makeDirectoryAndFiles(problem_number: str, file_formats: List[str]):
+    folder_name = problem_number
+    createFolder(folder_name)
+
+    createFiles(folder_name, problem_number, file_formats)
+
+
+def createFolder(folder_name: str):
+    if os.path.exists(folder_name):
+        return
+
+    os.makedirs(folder_name)
+
+
+def createFiles(folder_name: str, problem_number: str, file_formats: List[str]):
+    path = folder_name + '/' + problem_number + '.'
+    for file_format in file_formats:
+        file_path = path + file_format
+        if os.path.exists(file_path):
+            response = input(
+                f"Seems like the {file_path} file already exists!\n" + 
+                "Do you want to rewrite the file with a clear template?\n" + "(Y/n) "
+            )
+
+            if response != 'Y':
+                continue
+
+        writeTemplate(file_path, file_format)
+
+
+def writeTemplate(file_path: str, file_format: str):
+    with open(file_path, 'w') as file:
+        file.write("test")
+
