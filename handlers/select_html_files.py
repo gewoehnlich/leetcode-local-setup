48e6c8a05f05b7c46ccf13ccdf7813b844b7abf4
gewoehnlich <oomyse@gmail.com>
2025-01-18 01:22:37 +0500

    extended the getHTMLFiles() function

--- handlers/select_html_files.py
+++ handlers/select_html_files.py
@@ -1,18 +1,60 @@
 import tkinter as tk
 from tkinter import filedialog
+from pathlib import Path
 from exceptions.exceptions import NoFilesSelectedException
+from typing import List
+import json
 
-def getHTMLFiles():
+def getHTMLFiles() -> List[str]:
     root = tk.Tk()
     root.withdraw()
 
+    last_directory = getLastDirectory()
+
     file_paths = filedialog.askopenfilenames(
         title="Select files",
+        initialdir=last_directory,
         filetypes=[("HTML files", "*.html")]
     )
+
 
     if not file_paths:
         raise NoFilesSelectedException
 
+    last_directory = str(Path(file_paths[-1]).parent)
+    if last_directory:
+        saveLastDirectory(last_directory)
+
     return file_paths
+
+
+def getLastDirectory() -> None:
+    path = Path("config.json")    
+    if not path:
+        return False
+
+    with open(path, "r") as file:
+        data = json.load(file)
+        if not data.get("last_directory"):
+            return False
+        
+        return data.get("last_directory")
+
+
+def saveLastDirectory(directory: str) -> None:
+    path = Path("config.json")    
+    if not path:
+        return False
+    
+    data = {}
+    with open(path, "r") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            pass
+
+    data["last_directory"] = directory
+
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
 
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from handlers.select_html_files import getHTMLFiles()
+import sys   # remove later, used for debug
+from handlers.select_html_files import getHTMLFiles
 
 if __name__ == '__main__':
     html_files = getHTMLFiles()
+    sys.exit()
     for html in html_files:
         folder = createFolder(html)
         file = createFile(folder)
