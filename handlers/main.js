52de31b01f1be6ab6e61357fb48145e449c3ce1b
gewoehnlich <oomyse@gmail.com>
2025-02-13 01:00:03 +0500

    i keep doing stupid things

--- handlers/main.js
+++ handlers/main.js
@@ -8,8 +8,9 @@ import { formatTestcases } from "../formatters/format.js";
 export async function main() {
     const webpage = await getLeetcodeProblemPageHTML(); 
     const codeTemplate = await parseCodeTemplate(webpage);
-    const parsedTestcases = await parseTestcases(webpage);
-    console.dir(parsedTestcases);
+    // const defaultTemplate = parseDefaultCodeTemplate(webpage);
+    // const parsedTestcases = await parseTestcases(webpage);
+    // console.dir(parsedTestcases);
     // const fileformat = await getFileFormat(webpage);
     // const testcases = await formatTestcases(parsedTestcases, fileformat, codeTemplate);
     // const file = await compileFile(codeTemplate, testcases, fileformat);
